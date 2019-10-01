c52cc5bbaff3139aad0bf48180ac9f89d77d4bc1
Atiqah <atiqah@flowerpetalsofdoom.com>
2019-10-01 22:01:54 +0800

    fix age

--- current/assets/js/main.js
+++ current/assets/js/main.js
@@ -8,3 +8,20 @@ $('#main').t({
   
   }
 });
+
+function getAge() {
+  var dateString = "1994/06/16";
+  var today = new Date();
+  var birthDate = new Date(dateString);
+  var age = today.getFullYear() - birthDate.getFullYear();
+  var m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+  }
+  return age;
+}
+
+$(document).ready(function() { 
+  console.log(getAge());
+  $("#age").text( getAge() );
+});
